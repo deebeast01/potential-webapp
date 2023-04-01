ad59e39d76a80879971f1db53586b3d177bda4e5
deebeast01 <129584713+deebeast01@users.noreply.github.com>
2023-04-01 16:42:27 +0800

    Create homepage.php

---
+++ pages/admin/homepage.php
@@ -0,0 +1,56 @@
+<style>
+    .card{
+        width: 20%;
+        height: 150px;
+        border:2px solid red;
+        float:left;
+        position:relative;
+        margin-left:10%;
+        margin-top:12%;
+        border-radius: 25px;
+        box-shadow: 0 0 10px rgba(8,7,16,0.6);
+    }
+    .card:hover{
+        box-shadow: 0 0 20px rgba(8,7,16,0.6);
+    }
+    .upper{
+        width: 100%;
+        height:75%;
+        text-align: center;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-size:80px;
+        color:red;
+    }
+    .lower{
+        width: 100%;
+        height:25%;
+        
+    }
+    h1{
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        margin-left:2%;
+        color:red;
+    }
+    .lower{
+        background-color: red;
+        color:white;
+        text-align: center;
+        border-radius: 25px;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+</style>
+<?php
+
+    $result = mysqli_query($conn, "SELECT COUNT(emp_name) as total FROM tbl_employee");
+    $data = mysqli_fetch_assoc($result);
+
+    $result1 = mysqli_query($conn, "SELECT COUNT(appointment_id) as total1 FROM tbl_appointment");
+    $data1 = mysqli_fetch_assoc($result1);
+?>
+<div class = "title">
+    
+    <h1>Dashboard</h1>
+    <hr>
+</div>
+
+
