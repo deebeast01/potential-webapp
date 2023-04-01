24ceda1ff1398691374f99e823b978e6251045f1
deebeast01 <129584713+deebeast01@users.noreply.github.com>
2023-04-01 16:47:14 +0800

    Create services.php

---
+++ services/services.php
@@ -0,0 +1,173 @@
+
+<style>
+    .services-main-container{
+        margin-top:5%;
+        width:100%;
+        height:800px;
+        background-color:red;
+    }
+    .services-upper{
+       
+        width:100%;
+        height:8%;
+        
+        text-align:center;
+    }
+    .services-lower{
+        width:100%;
+        height:92%;
+        
+    }
+    .services-lower-left{
+        width:50%;
+        height:100%;
+        
+        float:left;
+    }
+    .services-lower-right{
+        width:50%;
+        height:100%;
+        
+        float:right;
+    }
+    .cell{
+        width: 70%;
+        height:15%;
+        border-radius:25px;
+        margin-left:15%;
+        margin-top:5%;
+        background-color:black;
+    }
+    .cell:hover{
+        box-shadow: 5px 5px 25px white;
+        cursor:pointer;
+    }
+    .cell-left{
+        border-radius:25px;
+        width:25%;
+        height:100%;
+        float:left;
+        background-color:white;
+        
+    }
+    .cell-right{
+        width:75%;
+        height:100%;
+        float:right;
+    }
+    .titulo{
+        color:white;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+    .back{
+        float:left;
+        
+    }
+    .backback{
+        color:white;
+        font-size:25px;
+        margin-left:100%;
+        float:left;
+    }
+    body{
+        background-color:red;
+    }
+</style>
+
+
+            
+<div class="services-main-container">
+
+    <div class="services-upper">
+        <div class="back">
+              <a class="backback" href="../index.php">Back</a>
+        </div>
+        <h1 class = "titulo">SERVICES</h1>
+    </div>
+    <div class="services-lower">
+        <div class="services-lower-left">
+            <div class="cell">
+                <div class="cell-left">
+                    <div class="divpic">
+
+                    </div>
+                </div>
+                <div class="cell-right">
+                        <h1 class ="titulo">MOBILE DIGITAL X-RAY</h1>
+                </div>
+            </div>
+            <div class="cell">
+                <div class="cell-left">
+                    <div class="divpic">
+
+                    </div>
+                </div>
+                <div class="cell-right">
+                        <h1 class ="titulo">CLINICAL LABORATORY</h1>
+                </div>
+            </div>
+            <div class="cell">
+                    <div class="cell-left">
+                        <div class="divpic">
+
+                        </div>
+                    </div>
+                    <div class="cell-right">
+                            <h1 class ="titulo">DRUG TESTING</h1>
+                    </div>
+            </div>
+            <div class="cell">
+                    <div class="cell-left">
+                                <div class="divpic">
+
+                                </div>
+                            </div>
+                            <div class="cell-right">
+                                    <h1 class ="titulo">ELECTROCARDIOGRAPHY</h1>
+                            </div>
+            </div>
+        </div>
+        <div class="services-lower-right">
+            <div class="cell">
+                    <div class="cell-left">
+                                        <div class="divpic">
+
+                                        </div>
+                                    </div>
+                                    <div class="cell-right">
+                                            <h1 class ="titulo">AUDIO-SCREENING</h1>
+                                    </div>
+            </div>
+            <div class="cell">
+                    <div class="cell-left">
+                                        <div class="divpic">
+
+                                        </div>
+                                    </div>
+                                    <div class="cell-right">
+                                            <h1 class ="titulo">ULTRASOUND</h1>
+                                    </div>
+            </div>
+            <div class="cell">
+                    <div class="cell-left">
+                                            <div class="divpic">
+
+                                            </div>
+                                        </div>
+                                        <div class="cell-right">
+                                                <h1 class ="titulo">PAPSMEAR</h1>
+                                        </div>
+            </div>
+            <div class="cell">
+                     <div class="cell-left">
+                                            <div class="divpic">
+
+                                            </div>
+                                        </div>
+                                        <div class="cell-right">
+                                                <h1 class ="titulo">HOUSE CALL</h1>
+                                        </div>
+            </div>
+        </div>
+    </div>
+</div>
